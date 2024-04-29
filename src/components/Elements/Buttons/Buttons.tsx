e37e31b1f420065442828c3c77ac743eac7159ad
Haile <haile4cmd@gmail.com>
2024-04-29 11:42:48 +0300

    build(Ellty-Test): create the button element

---
+++ src/components/Elements/Buttons/Buttons.tsx
@@ -0,0 +1,11 @@
+import './Button.css';
+
+const Buttons = () => {
+    return (
+        <button className='btn'>
+            Done
+        </button>
+    );
+};
+
+export default Buttons;
--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,9 +1,16 @@
+import { Button } from '../Elements';
 import './MainLayout.css';
 
 const MainLayout = () => {
     return (
         <div className="main-layout">
-            Hello world
+            <div className="main-frame">
+                <div className="main-card">
+                    Hello world
+                    <hr/>
+                    <Button/>
+                </div>
+            </div>
         </div>
     );
 };
