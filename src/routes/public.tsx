5fa8f678a3f1483b9958d881830a3aad93cc8cf6
Haile <haile4cmd@gmail.com>
2024-04-29 09:10:57 +0300

    build(Ellty-Test): handle route error

--- src/routes/public.tsx
+++ src/routes/public.tsx
@@ -1,14 +1,29 @@
 import {
     createBrowserRouter,
     RouterProvider,
+    useRouteError,
 } from 'react-router-dom';
+
+function ErrorPage() {
+    const error: unknown = useRouteError();
+    
+    return (
+        <div id="error-page">
+            <h1>Oops!</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <p>
+                <i>{(error as { statusText?: string })?.statusText || (error as Error)?.message}</i>
+            </p>
+        </div>
+    );
+}
 
 const PublicRoutes = () => {
     const router = createBrowserRouter([
         {
             path: '/',
             element: <div>Hello world!</div>,
-            errorElement: <div>error</div>,
+            errorElement: <ErrorPage />,
         },
     ]);
     return <RouterProvider router={router} />;
